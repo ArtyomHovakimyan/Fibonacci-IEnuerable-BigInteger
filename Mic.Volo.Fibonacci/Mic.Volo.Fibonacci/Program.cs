0b0665ea7c07e76ecd681bc850e1e074a66c1d9e
User <artyom.hovakimyan01@gmail.com>
2019-04-07 19:26:59 +0400

    add function and show result

--- Mic.Volo.Fibonacci/Mic.Volo.Fibonacci/Program.cs
+++ Mic.Volo.Fibonacci/Mic.Volo.Fibonacci/Program.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace Mic.Volo.Fibonacci
 {
     class Program
     {
+        static IEnumerable<BigInteger> Fibonacci()
+        {
+            BigInteger a = new BigInteger(0);
+            BigInteger b = new BigInteger(1);
+            BigInteger c = new BigInteger(0);
+
+            while (Console.ReadKey().Key == ConsoleKey.Spacebar)
+            {
+                yield return c;
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
         static void Main(string[] args)
         {
+            var Values = Fibonacci();
+            foreach (var value in Values)
+            {
+                Console.WriteLine($"Press Space to Print Next Value {value}");
+            }
         }
     }
 }
